abf14319bc64d7e3a5a74fb677c28a06072906df
gonzalez.yasir <gonzalez.yasir@SEMS-259-11.SEMS259.LAB>
2018-04-25 14:57:28 -0700

    PiAloud, String Methods1

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class PiAloud {
-
+	
 	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
+	}
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+String pi = "3.1415926535"
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
@@ -43,6 +44,7 @@ public class PiAloud {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
 	}
+
 
 }
 
--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -1,30 +1,33 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
-
 public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+String goldeneagle = "eagle";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.print(goldeneagle.charAt(3));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
-
+System.out.println(goldeneagle.length()==5);
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
-		
+for (int i = 0; i < goldeneagle.length(); i++) {
+System.out.println(goldeneagle.charAt(i));		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+}
+for (int i = 0; i < goldeneagle.length(); i++) {
+	if(goldeneagle.charAt(i)=='a') {
+		System.out.println("a is at index"+i);
 	}
+
 }
 
-
+}
+}
